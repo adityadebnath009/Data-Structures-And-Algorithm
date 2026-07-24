b18fee884c9884c13d076d2f00856eb4cc1a6f89
adityadebnath009 <debnathaditya2005@gmail.com>
2026-07-24 11:25:48 +0530

    LRU CACHE

--- LeetCode/Design/P0146_LruCache.java
+++ LeetCode/Design/P0146_LruCache.java
@@ -24,3 +24,105 @@ public class P0146_LruCache {
 
     // TODO: Write solution method here
 }
+class LRUCache {
+    class Node
+    {
+        Node prev;
+        Node next;
+        int key;
+        int value;
+        Node(int key, int value)
+        {
+            this.key = key;
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+    Map<Integer, Node> map;
+    int capacity;
+    Node head;
+    Node tail;
+
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head  = new Node(-1,-1);
+        tail = new Node(-1,-1);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if(!map.containsKey(key))
+        {
+            return -1;
+        }
+
+        Node temp = map.get(key);
+        removeFromDLL(temp);
+
+        addToFront(temp);
+        return temp.value;
+    }
+
+    public void put(int key, int value) {
+        Node temp = null;
+        if(map.containsKey(key))
+        {
+            temp = map.get(key);
+            temp.value = value;
+            removeFromDLL(temp);
+        }
+        else
+        {
+            temp = new Node(key, value);
+            map.put(key, temp);
+        }
+
+        //put at the front
+        addToFront(temp);
+
+        if(map.size() <= capacity)
+        {
+            return;
+        }
+        //cache is full
+        Node toBeRemoved = tail.prev;
+        if(map.containsKey(toBeRemoved.key))
+        {
+            map.remove(toBeRemoved.key);
+        }
+        //remove from DLL
+        removeFromDLL(toBeRemoved);
+
+
+    }
+    void addToFront(Node temp)
+    {
+        Node nextHead = head.next;
+
+        head.next = temp;
+        temp.prev = head;
+
+        temp.next = nextHead;
+        nextHead.prev = temp;
+
+    }
+    void removeFromDLL(Node temp)
+    {
+        Node prevNode = temp.prev;
+        Node nextNode = temp.next;
+
+        //de-link
+        temp.prev = null;
+        temp.next = null;
+
+        //merge the gap
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+    }
+}
+
+
